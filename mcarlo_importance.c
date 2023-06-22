c1279a98253d29361eca81af9101d78cf9cfd552
Gustavo Mangold <104363966+gustavomangold@users.noreply.github.com>
2023-06-22 03:18:34 -0300

    Update mcarlo_importance.c
    update identation
    fix code

--- mcarlo_importance.c
+++ mcarlo_importance.c
@@ -3,9 +3,7 @@
 #include <time.h>
 #include <stdlib.h>
 
-
 // distribuição uniforme
-
 double aleatorio()
 {
 	// Nao altere esta funcao!!!
@@ -14,37 +12,32 @@ double aleatorio()
 
 	x = a*x % m;
 
-	return ((double) x)/m; 
+	return ((double) x) / m; 
 }
 
 double f(double x){
-			return pow(x,1.5)*exp(-x);				
-
-				}
-
+	return pow(x,1.5)*exp(-x);				
+}
 
 double p(double x){
-			return 1.052*exp(-x);}
+	return 1.052*exp(-x);
+}
 
 #define N 100000
-void  main(){
+void main(){
 
         int i,j;
 	double soma;
 	double x[N], x2[N];
-	double count=0,INT=0, h,v, xmin=0., xmax=4., ymin=0., ymax=1.;
-	
-	
-	for(i=1; i<=N; i++){
-	x[i]= aleatorio();
-	x2[i]= -log(1- x[i]/1.052);
-	INT=INT+f(x2[i])/p(x2[i]);
+	double count = 0, INT = 0, h, v, xmin = 0., xmax = 4., ymin = 0., ymax = 1.;
+		
+	for(i = 1; i <= N; i++){
+	x[i] = aleatorio();
+	x2[i] = -log(1- x[i]/1.052);
+	INT = INT+f(x2[i])/p(x2[i]);
+		
+	printf("I = %lf\n", INT/N);
 }
-	
-			
-		printf("I = %lf\n",INT/N);
-
-		}
 
 
 											
