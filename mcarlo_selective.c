d105220feb87f4f8adf053659427e32b926a6c5e
Gustavo Mangold <104363966+gustavomangold@users.noreply.github.com>
2023-06-22 03:21:57 -0300

    Update mcarlo_selective.c
    update identitation and clear code

--- mcarlo_selective.c
+++ mcarlo_selective.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-#include<math.h>
-#include<time.h>
-#include<stdlib.h>
-
+#include <math.h>
+#include <time.h>
+#include <stdlib.h>
 
 // distribuição uniforme
-
 double aleatorio()
 {
 	// Nao altere esta funcao!!!
@@ -19,44 +17,31 @@ double aleatorio()
 
 double f(double x){
 			return pow(x,1.5)*exp(-x);				
-
-				}
-
+}
 
 #define N 100000
 void  main(){
-
         int i,j;
 	double x[N];
-	// Exemplo de uso da funcao para obter 2 numeros diferentes em [0:1)
 	
-	for(i=1; i<=N; i++){
-	x[i]= aleatorio();
-	x[i]= x[i]*3;
+	for(i = 1; i <= N; i++){
+		x[i] = aleatorio();
+		x[i] = x[i]*3;
 
 	//printf("%d %lf\n",i,x[i]);
+	}
 	
-	
+double INT, xmin = 0., xmax = 3., soma = 0, soma2 = 0, medio = 0, desvio = 0, variancia = 0;
+
+for(i = 1; i <= N; i++){				
+		soma = soma + f(x[i]);
+		medio = soma/i;
+		soma2 = soma2 + pow((f(x[i]) - medio), 2);
+		variancia = soma2/(i-1);
+		desvio = sqrt(variancia);	
 }
-	
-	// 1/N-1 soma(f(xi) - medio)²
-
-	double INT, xmin=0., xmax=3., soma=0, soma2=0, medio=0, desvio=0, variancia=0;
-
-		for(i=1;i<=N;i++){				
-				soma = soma + f(x[i]);
-				medio = soma/i;
-				soma2 = soma2 + pow((f(x[i]) - medio),2);
-				variancia = soma2/(i-1);
-				desvio = sqrt(variancia);
-				
-		INT=medio*(xmax-xmin);
-		
-						}
-		printf("I = %lf (+/-) %lf\n",INT, desvio);
-
-
-		}
+printf("I = %lf (+/-) %lf\n",medio*(xmax-xmin), desvio);
+}
 
 
 											
